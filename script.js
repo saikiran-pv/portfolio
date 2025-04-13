27dc6c7e7cd6f3e531af540c9972e1084bd89904
Saikiranreddy Peddavootla <saikiranreddypeddavootla@Mac.home.local>
2025-04-12 19:38:22 -0500

    adding remaining files

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const words = ["Software Engineer", "Ruby On Rails Developer", "Full Stack Developer", "Artist🖌️"];
+let index = 0;
+let letterIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+    let currentWord = words[index];
+
+    if (!isDeleting) {
+        letterIndex++;
+    } else {
+        letterIndex--;
+    }
+
+    document.getElementById("text").innerText = currentWord.substring(0, letterIndex);
+
+    if (!isDeleting && letterIndex === currentWord.length) {
+        setTimeout(() => isDeleting = true, 1000);
+    } else if (isDeleting && letterIndex === 0) {
+        isDeleting = false;
+        index = (index + 1) % words.length;
+    }
+
+    setTimeout(typeEffect, isDeleting ? 100 : 200);
+}
+
+document.addEventListener("DOMContentLoaded", typeEffect);
+
